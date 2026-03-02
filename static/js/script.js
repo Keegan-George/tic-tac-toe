ff7b5e01cf2a3c4b3547ecb3ace372800fe95e89
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2026-03-01 20:49:55 -0500

    Add validation to check each row of the board for a winner

--- static/js/script.js
+++ static/js/script.js
@@ -1,24 +1,32 @@
-const TicTacToe = (() => {
-    const gameBoard = [
+const GameBoard = (() => {
+    const gameboard = [
         ["", "", ""],
         ["", "", ""],
         ["", "", ""]
     ];
 
     const markPosition = (mark, xCoor, yCoor) => {
-        if (gameBoard[xCoor][yCoor] === "") {
-            gameBoard[xCoor][yCoor] = mark;
+        if (gameboard[xCoor][yCoor] === "") {
+            gameboard[xCoor][yCoor] = mark;
         }
 
         else {
-            console.log("Please choose another position. That position is already marked");
+            console.log("Please choose another position. This position is already marked.");
         }
-
-
     }
 
-    const showGameBoard = () => gameBoard;
+    const showGameBoard = () => gameboard;
+
+    function isWinner() {
+        //check each row for winner
+        for (let i = 0; i < gameboard.length; i++) {
+            if (gameboard[i].every(mark => mark !== "" && mark === gameboard[i][0])) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
-    return { markPosition, showGameBoard };
+    return { markPosition, showGameBoard, isWinner };
 })();
